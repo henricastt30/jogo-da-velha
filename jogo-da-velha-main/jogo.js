1970d0cc1ddb66077cd135f30900dbbef5f38bea
henricastt <moreirahenrique3010@gmail.com>
2025-03-18 15:24:23 -0300

    Mudando jogo da velha para damas

---
+++ jogo-da-velha-main/jogo.js
@@ -0,0 +1,24 @@
+let button, quebraLinha, jogada = 1, vencedor = 0
+let tabuleiro = new Array(8)
+for(let i = 0; i < tabuleiro.length; i++){
+    tabuleiro[i] = new Array(8)
+}
+for(let i = 0; i < tabuleiro.length; i++){
+    quebraLinha = document.createElement('br')
+    document.body.append(quebraLinha)
+    for(let j = 0; j < tabuleiro[i].length; j++){
+        button = document.createElement('button')
+        button.setAttribute('type','button')
+        button.setAttribute('id', 'bt' + i + "" + j)
+        button.setAttribute('class', 'btJogo' + i)
+        button.setAttribute('onclick', "marca(" + i + "," + j +")")
+        if(i % 2 == 1 && j % 2 == 0){
+            button.style.backgroundColor = "#000"
+        }
+        if(i % 2 == 0 && j % 2 == 1){
+            button.style.backgroundColor = "#000"
+        }
+        button.append(document.createTextNode(""))
+        document.body.append(button)
+    }
+}
